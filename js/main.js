c8ae4ba218e56fce31bbf2565216cee3b6505a5c
Louis <tetra_shine@hotmail.com>
2018-07-04 22:03:35 +0800

    Check clockwise, check intersect

--- js/main.js
+++ js/main.js
@@ -9,6 +9,16 @@ import LeafletGeometryUtil from "leaflet-geometryutil";
 import Button from '@material-ui/core/Button';
 import Snackbar from "@material-ui/core/Snackbar";
 
+let Angled = 0;
+let Parallel = 1;
+let CarParkWidth = 2.4;
+let Angled_Length = 4.8;
+let Parallel_Length = 5.4;
+let Angled_1Way_Aisle = 6;
+let Angled_2Way_Aisle = 6.6;
+let Parallel_1Way_Aisle = 3.6;
+let Parallel_2Way_Aisle = 6;
+
 firebase.initializeApp({
     apiKey: "AIzaSyCpQ67L2jxD_vsTjIiccgBr60sgC3LH7Yc",
     authDomain: "carpark-3fc4b.firebaseapp.com",
@@ -32,6 +42,17 @@ let street = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?a
 
 street.addTo(map);
 
+let cp = [];
+
+/*let cp =[
+    [1.3565410099139856, 103.82015903023031],
+    //[1.3565417332951493, 103.82015993165471],
+    [1.3563268127133283, 103.82051456694151],
+    [1.3562671680101281, 103.82051604011592],
+    [1.3562679091607217, 103.82015811158628],
+    [1.3565410099139856, 103.82015903023031]
+];*/
+
 class CarParkUi extends React.Component {
 
     constructor(props) {
@@ -43,32 +64,37 @@ class CarParkUi extends React.Component {
             snackMessage: "Start by drawing car park area on the map"
         };
 
-        this.polyline = new L.Draw.Polyline(map);
-        this.polyline.enable();
-        map.on(L.Draw.Event.CREATED, (event) => {
-            let layer = event.layer;
-            let latLngs = layer.getLatLngs();
-            let latLngsArr = processRoad(latLngs);
+        if (cp.length > 0) {
+            drawPolygon(cp, "red");
+            generateLots(cp);
+        } else {
+            this.polyline = new L.Draw.Polyline(map);
+            this.polyline.enable();
+            map.on(L.Draw.Event.CREATED, (event) => {
+                let layer = event.layer;
+                let latLngs = layer.getLatLngs();
+                let latLngsArr = processRoad(latLngs);
 
-            this.setState({
-                start: false,
-                showSnackbar: false,
-                action: <div>
-                    <Button size="small" variant="contained" onClick={this.onSave.bind(this)}>Save</Button>
-                    <Button size="small" variant="contained" onClick={this.onRetry.bind(this)}>Try Again</Button>
-                </div>,
-                snackMessage: "Click on 'Save' if the generated car park has any issues",
-                carpark: latLngs
+                this.setState({
+                    start: false,
+                    showSnackbar: false,
+                    action: <div>
+                        <Button size="small" variant="contained" onClick={this.onSave.bind(this)}>Save</Button>
+                        <Button size="small" variant="contained" onClick={this.onRetry.bind(this)}>Try Again</Button>
+                    </div>,
+                    snackMessage: "Click on 'Save' if the generated car park has any issues",
+                    carpark: latLngs
+                });
+                this.polyline.disable();
+
+                drawPolygon(latLngsArr, "red");
+                generateLots(latLngsArr);
+
+                this.setState({
+                    showSnackbar: true
+                });
             });
-            this.polyline.disable();
-
-            drawPolygon(latLngsArr, "red");
-            generateLots(latLngsArr);
-
-            this.setState({
-                showSnackbar: true
-            });
-        });
+        }
     }
 
     onSave() {
@@ -152,15 +178,7 @@ map.fitBounds(cp1.getBounds());*/
 //      2. place road
 //      3. cut polygon off from external road border
 //      4. recusively call placeLots using remaining area
-const Angled = 0;
-const Parallel = 1;
-const CarParkWidth = 2.4;
-const Angled_Length = 4.8;
-const Parallel_Length = 5.4;
-const Angled_1Way_Aisle = 6;
-const Angled_2Way_Aisle = 6.6;
-const Parallel_1Way_Aisle = 3.6;
-const Parallel_2Way_Aisle = 6;
+
 
 function drawGeojson(geojson) {
     let x = L.geoJSON(geojson, {});
@@ -255,99 +273,125 @@ function calcBufferDistance(a, b, c, length = Angled_Length) {
 //  1.3 calculate distance of line and find the largest number of lots to placeLots
 //  1.4 place lots in the center of the line
 //2. Merge all roads
-function generateLots(points) {
+function generateLots(points, skip=false) {
     if (points.length <= 2) return;
+    let union;
     let first = points[0], second, third, start, end, startDistance = 0, roadCoord1, roadCoord2;
     let len = points.length;
     let lots = [], roads = [];
+    let clockwise = turf.booleanClockwise(points);
 
     //1. for each line
-    points.forEach((point, index) => {
-        if (index == 0) return;
+    if (!skip) {
+        points.forEach((point, index) => {
+            if (index == 0) return;
 
-        second = point;
-        third = (index + 1 < len) ? points[((index + 1) % len)] : points[1];
+            second = point;
+            third = (index + 1 < len) ? points[((index + 1) % len)] : points[1];
 
-        let firstLatLng = toLatLng(first);
-        let secondLatLng = toLatLng(second);
-        let thirdLatLng = toLatLng(third);
+            let firstLatLng = toLatLng(first);
+            let secondLatLng = toLatLng(second);
+            let thirdLatLng = toLatLng(third);
 
-        start = firstLatLng;
-        end = secondLatLng;
+            start = firstLatLng;
+            end = secondLatLng;
 
-        let bearing = L.GeometryUtil.bearing(firstLatLng, secondLatLng);
-        let reverseBearing = L.GeometryUtil.bearing(secondLatLng, firstLatLng);
+            let bearing = L.GeometryUtil.bearing(firstLatLng, secondLatLng);
+            let reverseBearing = L.GeometryUtil.bearing(secondLatLng, firstLatLng);
 
-        //1.1 find the relational start point of placing lot so that next line can continue to place lots
-        let calcStart;
-        if (index == 1) {
-            startDistance = calcBufferDistance(points[len - 2], first, second, Angled_Length);
-        }
-
-        calcStart = L.GeometryUtil.destination(start, bearing, startDistance);
-
-        //1.2 find the relational end point of placing lot so that next line can continue to place lots
-        let subDistance = calcBufferDistance(first, second, third, Angled_Length);
-        let calcEnd = L.GeometryUtil.destination(end, reverseBearing, subDistance);
-
-        startDistance = subDistance;
-
-        //1.3 calculate distance of line and find the largest number of lots to place
-        let startLatLng = calcStart;
-        let endLatLng = calcEnd;
-        let distance = startLatLng.distanceTo(endLatLng);
-        let maxLots = Math.floor(distance / CarParkWidth);
-        let remainingHalfDist = (distance - (maxLots * CarParkWidth)) / 2;
-
-        //1.4 place lots in the center of the line
-        let cpStart = L.GeometryUtil.destination(startLatLng, bearing, remainingHalfDist);
-
-        for (let i = 0; i < maxLots; i++) {
-
-            let cpEnd = L.GeometryUtil.destination(cpStart, bearing, CarParkWidth);
-            let lot = generateAngledLot(cpStart, cpEnd);
-            lots.push(lot);
-            drawPolygon(lot);
-
-            if (i == 0) {
-                roadCoord1 = lot[3];
+            //1.1 find the relational start point of placing lot so that next line can continue to place lots
+            let calcStart;
+            if (index == 1) {
+                startDistance = calcBufferDistance(points[len - 2], first, second, Angled_Length);
             }
 
-            if (i == maxLots - 1) {
-                roadCoord2 = lot[2];
+            calcStart = L.GeometryUtil.destination(start, bearing, startDistance);
+
+            //1.2 find the relational end point of placing lot so that next line can continue to place lots
+            let subDistance = calcBufferDistance(first, second, third, Angled_Length);
+            let calcEnd = L.GeometryUtil.destination(end, reverseBearing, subDistance);
+
+            startDistance = subDistance;
+
+            //1.3 calculate distance of line and find the largest number of lots to place
+            let startLatLng = calcStart;
+            let endLatLng = calcEnd;
+            let distance = startLatLng.distanceTo(endLatLng);
+            let maxLots = Math.floor(distance / CarParkWidth);
+            let remainingHalfDist = (distance - (maxLots * CarParkWidth)) / 2;
+
+            //1.4 place lots in the center of the line
+            let cpStart = L.GeometryUtil.destination(startLatLng, bearing, remainingHalfDist);
+
+            for (let i = 0; i < maxLots; i++) {
+
+                let cpEnd = L.GeometryUtil.destination(cpStart, bearing, CarParkWidth);
+                let lot = generateAngledLot(cpStart, cpEnd, clockwise);
+                lots.push(lot);
+                drawPolygon(lot);
+
+                if (i == 0) {
+                    roadCoord1 = lot[3];
+                }
+
+                if (i == maxLots - 1) {
+                    roadCoord2 = lot[2];
+                }
+
+                cpStart = cpEnd;
             }
 
-            cpStart = cpEnd;
-        }
+            let road = generateRoad(roadCoord1, roadCoord2);
+            roads.push(road);
 
-        let road = generateRoad(roadCoord1, roadCoord2);
-        roads.push(road);
-
-        first = second;
-    });
-
-    //2. Merge all roads
-    let turfPolygons = roads.map(road => {
-        let points = road.map(point => {
-            return [point.lat, point.lng];
+            first = second;
         });
-        points.push(points[0]);
-        return turf.polygon([points]);
-    });
 
-    let union = turfPolygons[0];
-    for(let i = 1; i < turfPolygons.length; i++) {
-        union = turf.union(union, turfPolygons[i]);
+        //2. Merge all roads
+        let turfPolygons = roads.map(road => {
+            let points = road.map(point => {
+                return [point.lat, point.lng];
+            });
+            points.push(points[0]);
+            return turf.polygon([points]);
+        });
+
+        union = turfPolygons[0];
+        for (let i = 1; i < turfPolygons.length; i++) {
+            union = turf.union(union, turfPolygons[i]);
+        }
+    } else {
+        union = turf.polygon([points]);
     }
 
-    let p, road, turfPolygon;
-    for (let i = 1; i < union.geometry.coordinates.length; i++) {
-        road = generateFullLots(union.geometry.coordinates[i]);
 
-        if (!road) { break; }
+    let p, road, turfPolygon, coords;
+    for (let i = skip ? 0 : 1; i < union.geometry.coordinates.length; i++) {
+        coords = union.geometry.coordinates[i];
+        clockwise = turf.booleanClockwise(coords);
+
+        for (let j = 0, end = coords.length - 2; j < end; j++) {
+            road = generateFullLots(coords, clockwise, j, j+1);
+
+            if (road) {
+                p = processRoad(road);
+                turfPolygon = turf.polygon([p]);
+                union = turf.union(union, turfPolygon);
+
+                break;
+            }
+        }
+
+        /*road = generateFullLots(coords, clockwise);
+
+        if (!road) {
+            console.log(coords);
+            break;
+        }
+
         p = processRoad(road);
         turfPolygon = turf.polygon([p]);
-        union = turf.union(union, turfPolygon);
+        union = turf.union(union, turfPolygon);*/
     }
 
     drawGeojson(turf.flip(union));
@@ -370,19 +414,19 @@ function reversePoint(point) {
 //1. start with first 2 points,
 //2. find point to start draw and point to end
 //3. calc max number and populate them
-function generateFullLots(area, draw=false) {
+function generateFullLots(area, clockwise=false, id0=0, id1=1) {
 
     let road;
     try {
-        let first = area[0];
-        let second = area[1];
+        let first = area[id0];
+        let second = area[id1];
 
         //1. start with first 2 points,
         //2. find point to start draw and point to end
         //      2.1 find line before (line1) and after (line2)
-        let line = findLineFromArea(area, 0);
-        let line1 = findLineFromArea(area, 1);
-        let line2 = reversePoint(findLineFromArea(area, -1));
+        let line = findLineFromArea(area, id0);
+        let line1 = findLineFromArea(area, id1);
+        let line2 = reversePoint(findLineFromArea(area, id0-1));
 
         //      2.2 find corresponding start and end point
         let angle1 = findAngleBetween(line, line2);
@@ -406,13 +450,13 @@ function generateFullLots(area, draw=false) {
         //3. calc max number and populate them
         let lots, next;
 
-        [lots, next] = generateHorizontalAngledLots(first, second);
+        [lots, next] = generateHorizontalAngledLots(first, second, clockwise);
         if (lots.length <= 0) {
             return null;
         }
 
         next = nextToIntersect(next, line1, line2);
-        [lots, next] = generateHorizontalAngledLots(...next);
+        [lots, next] = generateHorizontalAngledLots(...next, clockwise);
         if (lots.length <= 0) {
             return null;
         }
@@ -421,21 +465,23 @@ function generateFullLots(area, draw=false) {
 
         road = generateRoad(...next, false);
 
+        //from new road coords, continue extend to hit line1 & 2
+        let newRoadCoords = nextToIntersect([road[2], road[3]], line1, line2);
+
         let bearing1 = L.GeometryUtil.bearing(road[0], road[1]);
         let revBearing1 = L.GeometryUtil.bearing(road[1], road[0]);
-        let bearing2 = L.GeometryUtil.bearing(road[2], road[3]);
-        let revBearing2 = L.GeometryUtil.bearing(road[3], road[2]);
+        let bearing2 = L.GeometryUtil.bearing(newRoadCoords[1], newRoadCoords[0]);
+        let revBearing2 = L.GeometryUtil.bearing(newRoadCoords[0], newRoadCoords[1]);
         let ext1 = L.GeometryUtil.destination(road[1], bearing1, Angled_1Way_Aisle / 2);
         let ext0 = L.GeometryUtil.destination(road[0], revBearing1, Angled_1Way_Aisle / 2);
-        let ext3 = L.GeometryUtil.destination(road[3], bearing2, Angled_1Way_Aisle / 2);
-        let ext2 = L.GeometryUtil.destination(road[2], revBearing2, Angled_1Way_Aisle / 2);
+        let ext3 = L.GeometryUtil.destination(newRoadCoords[0], bearing2, Angled_1Way_Aisle / 2);
+        let ext2 = L.GeometryUtil.destination(newRoadCoords[1], revBearing2, Angled_1Way_Aisle / 2);
         road = [ext0, ext1, ext2, ext3];
     } catch (ex) {
         console.log("error occurred");
         console.log(ex);
-        drawPolygon(area, "red");
+        //drawPolygon(area, "red");
     }
-
 
     return road;
 }
@@ -457,30 +503,52 @@ function getPerpendicularDestination(map, from, point2, distance, clockwise) {
 
 //find the line that intersect line1 & line2 by continuing line next
 function nextToIntersect(next, line1, line2) {
-
-    let coords = extendLineCoords(next);
     line1 = extendLineCoords([toLatLng(line1[0]), toLatLng(line1[1])]);
     line2 = extendLineCoords([toLatLng(line2[0]), toLatLng(line2[1])]);
 
-    let turfLine = turf.lineString(coords);
     let turfLine1 = turf.lineString(line1);
     let turfLine2 = turf.lineString(line2);
-    let intersect1 = turf.lineIntersect(turfLine, turfLine1);
-    let intersect2 = turf.lineIntersect(turfLine, turfLine2);
 
-    return [toLatLng(intersect2.features[0].geometry.coordinates), toLatLng(intersect1.features[0].geometry.coordinates)];
+    let intersect1Coords = extendLineTillIntersect(next, turfLine1);
+    let intersect2Coords = extendLineTillIntersect(next, turfLine2);
+
+    if (!intersect1Coords) {
+        intersect1Coords = [next[0].lat, next[0].lng];
+    }
+
+    if (!intersect2Coords) {
+        intersect2Coords = [next[1].lat, next[1].lng];
+    }
+
+    return [toLatLng(intersect2Coords), toLatLng(intersect1Coords)];
 }
 
-function extendLineCoords(line, by=Angled_Length) {
+function extendLineTillIntersect(line, turfIntersect, repeat=5) {
+    let extended = line, turfLine, intersect;
+    let n = 0;
+    while(n++ < repeat) {
+        extended = extendLineCoords(extended, Angled_Length, false);
+        turfLine = turf.lineString(extended.map(_ => {
+            return [_.lat, _.lng];
+        }));
+        intersect = turf.lineIntersect(turfLine, turfIntersect);
+
+        if (intersect.features.length > 0) {
+            return intersect.features[0].geometry.coordinates;
+        }
+    }
+}
+
+function extendLineCoords(line, by=Angled_Length, retInCoords=true) {
     let bearing = L.GeometryUtil.bearing(...line);
     let revBearing = L.GeometryUtil.bearing(line[1], line[0]);
 
     let ext2 = L.GeometryUtil.destination(line[1], bearing, by);
     let ext1 = L.GeometryUtil.destination(line[0], revBearing, by);
 
-    return [ext1, ext2].map(_ => {
+    return retInCoords ? [ext1, ext2].map(_ => {
         return [_.lat, _.lng];
-    });
+    }) : [ext1, ext2];
 }
 
 function findLineFromArea(area, index) {
